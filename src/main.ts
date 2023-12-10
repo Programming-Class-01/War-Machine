c71a4e5204ac811bbf5fda7503c338d401834c3b
tokzillu <Link345670@yahoo.com>
2023-12-09 21:38:41 -0600

    Experimenting with decks and shuffling

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,44 @@
 console.log('Hello WARld')
+
+interface ICard {
+    uuid: uuid,
+    value: (0-12),
+    suit: (0-3)
+  }
+
+var suits = ["spades", "diamonds", "clubs", "hearts"];
+var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+function makeDeck()
+{
+	let deck = new Array();
+
+	for(let i = 0; i < suits.length; i++)
+	{
+		for(let x = 0; x < values.length; x++)
+		{
+			let card = {Value: values[x], Suit: suits[i]};
+			deck.push(card);
+		}
+	}
+
+	return deck;
+}
+
+makeDeck()
+
+
+function shuffle(deck)
+{
+	for (let i = 0; i < 1000; i++)
+	{
+		let location1 = Math.floor((Math.random() * deck.length));
+		let location2 = Math.floor((Math.random() * deck.length));
+		let tmp = deck[location1];
+
+		deck[location1] = deck[location2];
+		deck[location2] = tmp;
+	}
+}
+
+shuffle
