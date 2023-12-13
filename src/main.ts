45241e405f2c127229a0ec85d983a1e4ac95b23e
tokzillu <Link345670@yahoo.com>
2023-12-12 22:17:20 -0600

    Updates and changes to the interface and function

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,35 @@
 console.log('Hello WARld')
 
 interface ICard {
-    value: typeof values;
-    suit: typeof suits;
-	face: typeof faces;
-  }
+	value: number;
+	suit: string;
+	face: string;
+}
 
-  interface IDeck {
-	owner: typeof owner;
+interface IDeck {
+	owner: number;
 	cardcount: ICard;
-  }
+}
 
 var owner = [1, 2];
 var suits = ["spades", "diamonds", "clubs", "hearts"];
 var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 var faces = ["Ace", "Jack", "Queen", "King"];
 
-function makeDeck(): IDeck {
-	let deck = new Array()
-		for(let i = 0; i < suits.length; i++)
-	{
-		for(let x = 0; x < values.length; x++)
-		{
-			let card = {Value: values[x], Suit: suits[i]};
-			deck.push(card);
-		}
+function makeDeck(deck: IDeck) {
+	const newDeck: IDeck[] = [];
+	for (let i = 0; i < suits.length; i++) {
+		for (let x = 0; x < values.length; x++)
+			for (let c = 0; c < faces.length; c++) {
+				const card: ICard = { Value: values[x], Suit: suits[i], Face: faces[c] };
+				newDeck.push(card);
+			}
 	}
 
 	return deck;
 
 }
+makeDeck
 
 
 
@@ -52,10 +52,8 @@ function makeDeck(): IDeck {
 // makeDeck()
 
 
-function shuffle(deck)
-{
-	for (let i = 0; i < 1000; i++)
-	{
+function shuffle(deck) {
+	for (let i = 0; i < 1000; i++) {
 		let location1 = Math.floor((Math.random() * deck.length));
 		let location2 = Math.floor((Math.random() * deck.length));
 		let tmp = deck[location1];
