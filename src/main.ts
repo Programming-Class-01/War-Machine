ecc8a8a640ba76a2683301906365ef153deda9be
tokzillu <Link345670@yahoo.com>
2023-12-14 16:03:26 -0600

    Minor changes that accomplish nothing

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 console.log('Hello WARld')
 
 interface ICard {
-	value: number;
+	worth: number;
 	suit: string;
 	face: string;
 }
@@ -13,17 +13,18 @@ interface IDeck {
 
 var owner = [1, 2];
 var suits = ["spades", "diamonds", "clubs", "hearts"];
-var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+var worths = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 var faces = ["Ace", "Jack", "Queen", "King"];
 
 function makeDeck(deck: IDeck) {
 	const newDeck: IDeck[] = [];
 	for (let i = 0; i < suits.length; i++) {
-		for (let x = 0; x < values.length; x++)
-			for (let c = 0; c < faces.length; c++) {
-				const card: ICard = { Value: values[x], Suit: suits[i], Face: faces[c] };
-				newDeck.push(card);
-			}
+		for (let x = 0; x < worths.length; x++)
+			for (let c = 0; c < faces.length; c++)
+				if (typeof worths === `undefined`) return Error(`shit's fucked bruh`) {
+			const card: ICard = { worth: worths[x], Suit: suits[i], Face: faces[c] };
+			newDeck.push(card);
+		}
 	}
 
 	return deck;
@@ -32,24 +33,6 @@ function makeDeck(deck: IDeck) {
 makeDeck
 
 
-
-// function makeDeck()
-// {
-// 	let deck = IDeck;
-
-// 	for(let i = 0; i < suits.length; i++)
-// 	{
-// 		for(let x = 0; x < values.length; x++)
-// 		{
-// 			let card = {Value: values[x], Suit: suits[i]};
-// 			deck.push(card);
-// 		}
-// 	}
-
-// 	return deck;
-// }
-
-// makeDeck()
 
 
 function shuffle(deck) {
